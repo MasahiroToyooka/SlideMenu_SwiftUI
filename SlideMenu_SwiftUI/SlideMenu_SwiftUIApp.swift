52140a6e455bfcccfd8bdf4483f07519aa40a49d
MasahiroToyooka <ehara2481@gmail.com>
2020-10-31 09:51:20 +0900

    Initial Commit

---
+++ SlideMenu_SwiftUI/SlideMenu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenu_SwiftUIApp.swift
+//  SlideMenu_SwiftUI
+//
+//  Created by 豊岡正紘 on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
